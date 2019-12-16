406264c6c8dbc16cf95d25808351208d94fd3fde
Jennifer Gomez <jennifergomezbruno@gmail.com>
2019-12-16 09:37:15 -0500

    Updated

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,11 +1,32 @@
 class Genre
 
-    attr_accessor :name 
+  attr_accessor :name 
 
-    def initialize(name)
-        @name = name 
+  @@all = []
 
+  def initialize(name)
+      @name = name 
+      @@all << self
+  end
+
+  def self.all
+      @@all
+  end
+
+  def songs
+      #has many songs
+      Song.all.select {|song| song.genre == self}
+  end
+
+  def artists 
+    #has many artists, through songs
+    songs.map do |song|
+      song.artist 
     end
+  end
 
+  def add_song(song)
+      songs << song
+  end
 
 end
